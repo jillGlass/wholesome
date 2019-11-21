1810dd40f42a2c253217aae74409b2d4af331b6e
Roman Mitch <romanmitch@gmail.com>
2019-11-22 11:11:36 +1300

    getPromptById made

--- server/db/db.js
+++ server/db/db.js
@@ -1,24 +1,11 @@
-/*
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../../knexfile')[environment]
+const connection = require('knex')(config)
 
-Never have I ever:
+module.exports = {
+  getPromptById
+}
 
-been kicked out of a bar
-been to Australia
-git rebased when I should't have
-thrown my laptop at a mate
-performed at the civic
-poured tea on my laptop
-fallen asleep during a lecture
-been a member of a Glee club
-done a walk of shame (aka stride of pride)
-lived in a tiny house
-shoplifted
-been arrested
-gotten stitches
-gone vegan
-bungy jumped
-dined and dashed
-jumped from a roof
-had a paranormal experience
-had to drink the king cup
-
+function getPromptById (id, db = connection) {
+  return db('prompt').where('id', id).select()
+}
