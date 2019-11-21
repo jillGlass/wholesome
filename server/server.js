489d04b9a8136f533294c3b49e535d7d435e878d
Roman Mitch <romanmitch@gmail.com>
2019-11-22 11:19:24 +1300

    add router in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const express = require('express')
+const db = require('./db/db')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-module.exports = server
+server.get('/:id', (req, res) => {
+  const promptId = Number(req.params.id)
+  db.getPromptById(promptId)
+    .then(p => res.json(p))
+    .catch(err => res.status(500).send('Never have I ever had a database error' + err.message))
+})
