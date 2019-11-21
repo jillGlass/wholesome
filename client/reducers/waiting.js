445d7d2e07f29a49f0631d8e2c267dea27e7a3ee
Jill Glass <glass.nz@gmail.com>
2019-11-22 11:44:19 +1300

    reducers and actions completed

---
+++ client/reducers/waiting.js
@@ -0,0 +1,15 @@
+import { REQUEST_PROMPT, SHOW_ERROR, RECEIVE_PROMPT } from '../actions'
+
+export default function waiting (state = false, action) {
+  switch (action.type) {
+    case REQUEST_PROMPT:
+      return true
+    case RECEIVE_PROMPT:
+      return false
+    case SHOW_ERROR:
+      return false
+
+    default:
+      return state
+  }
+}
