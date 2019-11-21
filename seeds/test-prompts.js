8379980d9e107d412228f10940b0d8c73040529a
Roman Mitch <romanmitch@gmail.com>
2019-11-22 11:05:10 +1300

    add seeds

--- seeds/test-prompts.js
+++ seeds/test-prompts.js
@@ -1,13 +1,29 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('prompt').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+      return knex('prompt').insert([
+        { id: 1, promptItem: 'been kicked out of a bar' },
+        { id: 2, promptItem: 'been to Stralia' },
+        { id: 3, promptItem: 'thought it was a good idea to git rebase' },
+        { id: 4, promptItem: 'been kicked out of a bar' },
+        { id: 5, promptItem: 'thrown my laptop at a mate' },
+        { id: 6, promptItem: 'performed at the Civic' },
+        { id: 7, promptItem: 'poured tea on my laptop' },
+        { id: 8, promptItem: 'fallen asleep during a lecture' },
+        { id: 9, promptItem: 'been a member of a Glee club' },
+        { id: 10, promptItem: 'done a walk of shame (aka stride of pride)' },
+        { id: 11, promptItem: 'lived in a tiny house' },
+        { id: 12, promptItem: 'shoplifted' },
+        { id: 13, promptItem: 'been arrested' },
+        { id: 14, promptItem: 'gotten stitches' },
+        { id: 15, promptItem: 'gone Vegan' },
+        { id: 16, promptItem: 'bungy jumped' },
+        { id: 17, promptItem: 'dinned and dashed' },
+        { id: 18, promptItem: 'jumped off a roof' },
+        { id: 19, promptItem: 'had a paranormal experience' },
+        { id: 20, promptItem: 'had to drink the king cup' }
+      ])
+    })
 };
