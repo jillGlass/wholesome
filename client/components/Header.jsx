7ec43df9da19a70c9376a332450c3116093acb45
Jill Glass <glass.nz@gmail.com>
2019-11-22 12:10:09 +1300

    header component is coming along nicely

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,14 +1,20 @@
-
-
-
 // use the below class names for the header, which is basically a nav bar. INSTEAD OF BURGER   i think we need to use nav-center
-/*
-<nav>
-  <ul class="navigation">
-    <div class="nav-right">
-      <li class="title-eda size"><a href="/" class="burger">NEVER</a></li>
-      <li class="title-eda"><a href="/list" class="burger"></a></li>
-    </div>
-  </ul>
-</nav>
-*/
+import React from 'react'
+
+class Header extends React.Component {
+  render () {
+    return (
+      <nav>
+        <ul className="navigation">
+          <div className="nav-right">
+            <h1 className="burger">NEVER</h1>
+          
+          </div>
+        </ul>
+      </nav>
+
+    )
+  }
+}
+
+export default Header
